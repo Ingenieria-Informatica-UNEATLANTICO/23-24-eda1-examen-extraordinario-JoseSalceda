ab56d5939ff2e996085da0b8b928c4c57d719d79
JoseSalceda <114105383+JoseSalceda@users.noreply.github.com>
2024-07-05 17:00:12 +0200

    Update Restaurante.java to add pruebas

--- src/Restaurante.java
+++ src/Restaurante.java
@@ -1,6 +1,6 @@
 package src;
 
-public class Restaurante {
+class Restaurante {
     private HamburguesaManager hamburguesaManager;
 
     public Restaurante() {
@@ -10,39 +10,44 @@ public class Restaurante {
     private void atender() {
         Cocinero bobEsponja = new Cocinero(hamburguesaManager);
         Camarero calamardo = new Camarero();
-
         bobEsponja.prepararHamburguesa();
         calamardo.recibir(bobEsponja.entregar());
         calamardo.servir();
     }
 
+    private void demostrarFuncionalidad() {
+        System.out.println("Demostrando funcionalidad del sistema:");
+
+        for (int i = 0; i < 3; i++) {
+            atender();
+            System.out.println("Hamburguesa " + (i + 1) + " creada y servida.");
+        }
+
+        System.out.println("\nCantidad de hamburguesas: " + hamburguesaManager.obtenerCantidadHamburguesas());
+
+        System.out.println("\nTodas las hamburguesas:");
+        Hamburguesa[] todasLasHamburguesas = hamburguesaManager.obtenerTodasLasHamburguesas();
+        for (int i = 0; i < todasLasHamburguesas.length; i++) {
+            System.out.println("Hamburguesa " + (i + 1) + ": " + todasLasHamburguesas[i].describir());
+        }
+
+        if (hamburguesaManager.obtenerCantidadHamburguesas() > 0) {
+            Hamburguesa nuevaHamburguesa = new Hamburguesa(new Integral(), new Integral(), new Pollo("bien hecho"), 2);
+            hamburguesaManager.actualizarHamburguesa(0, nuevaHamburguesa);
+            System.out.println("\nHamburguesa actualizada: " + hamburguesaManager.obtenerHamburguesa(0).describir());
+        }
+
+        if (hamburguesaManager.obtenerCantidadHamburguesas() > 0) {
+            hamburguesaManager.eliminarHamburguesa(hamburguesaManager.obtenerCantidadHamburguesas() - 1);
+            System.out.println("\nÚltima hamburguesa eliminada.");
+            System.out.println("Nueva cantidad de hamburguesas: " + hamburguesaManager.obtenerCantidadHamburguesas());
+        }
+    }
+
     public static void main(String[] args) {
         Restaurante crustaceoCrujiente = new Restaurante();
         crustaceoCrujiente.atender();
-    }
-
-    public void crearNuevaHamburguesa(Pan panSuperior, Pan panInferior, Carne carne, int numeroExtras) {
-        hamburguesaManager.crearHamburguesa(panSuperior, panInferior, carne, numeroExtras);
-    }
-
-    public Hamburguesa obtenerHamburguesa(int index) {
-        return hamburguesaManager.obtenerHamburguesa(index);
-    }
-
-    public void actualizarHamburguesa(int index, Hamburguesa hamburguesa) {
-        hamburguesaManager.actualizarHamburguesa(index, hamburguesa);
-    }
-
-    public void eliminarHamburguesa(int index) {
-        hamburguesaManager.eliminarHamburguesa(index);
-    }
-
-    public void mostrarTodasLasHamburguesas() {
-        Hamburguesa[] hamburguesas = hamburguesaManager.obtenerTodasLasHamburguesas();
-        for (Hamburguesa hamburguesa : hamburguesas) {
-            System.out.println(hamburguesa.describir());
-            hamburguesa.mostrar();
-            System.out.println();
-        }
+        System.out.println("\n--- Demostración adicional ---\n");
+        crustaceoCrujiente.demostrarFuncionalidad();
     }
 }
